a3e6eb9255b51601c5f0918b00b33872b73f9542
michaelonyedika <michaelonyedika32@gmail.com>
2025-07-16 12:46:17 +0100

    Implement Editor-top-bar

---
+++ src/assets/icons/Preview.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const Preview = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="11"
+    height="12"
+    fill="currentColor"
+    viewBox="0 0 11 12"
+  >
+    <path
+      fill="currentColor"
+      d="M9.58 5.163q.247.167.37.367a.87.87 0 0 1 .124.467.87.87 0 0 1-.123.466 1.2 1.2 0 0 1-.37.367l-7.271 4.4a1.4 1.4 0 0 1-.283.1 1.2 1.2 0 0 1-.282.033q-.424 0-.741-.266-.318-.267-.318-.734V1.63q0-.467.318-.733.317-.267.74-.267.142 0 .283.033.14.034.283.1zM1.674 1.53v8.933l7.483-4.466z"
+    ></path>
+  </svg>
+);
+
+export default Preview;
