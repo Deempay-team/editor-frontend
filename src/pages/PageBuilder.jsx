4f70eaaf9541bf5dfacd5c9b46be9740c8653358
peter-godwin <godwinapeter@gmail.com>
2025-08-07 13:27:24 +0300

    remove header

--- src/pages/PageBuilder.jsx
+++ src/pages/PageBuilder.jsx
@@ -54,7 +54,6 @@ export default function PageBuilder() {
           AnnouncementBarRender ,
           NavigationBar,
           Grid,
-          Header,
         }}
         onRender={RenderNode}
       >
