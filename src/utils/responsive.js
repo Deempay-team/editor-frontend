8ebf4d2749eaf682c894fccf4b08995c392a8b0b
michaelonyedika <michaelonyedika32@gmail.com>
2025-09-24 11:19:52 +0100

    Product collection

---
+++ src/utils/responsive.js
@@ -0,0 +1,29 @@
+// Responsive functions for height & width
+export function getResponsiveProp({
+  isDesktop,
+  propMode = "custom",
+  propSize,
+  propModeMobile,
+  propSizeMobile,
+}) {
+  // Decide which props to use (desktop vs mobile)
+  const mode = isDesktop ? propMode : propModeMobile ?? propMode;
+  const custom = isDesktop ? propSize : propSizeMobile ?? propSize;
+
+  if (mode === "auto") return "auto";
+  if (mode === "full") return "100%";
+  if (mode === "custom" && custom) return `${custom}px`;
+
+  return "auto"; // fallback
+}
+
+// Responsive functions for fontSize, fontWeight, paddingX, paddingY etc.
+export function getResponsivePropSize(isDesktop, propSize, propSizeMobile) {
+  if (isDesktop) {
+    return propSize ? `${propSize}px` : null;
+  }
+  if (propSizeMobile) {
+    return `${propSizeMobile}px`;
+  }
+  return propSize ? `${propSize}px` : null;
+}
